89016bcda81a1d1384c1ce2c9092f9c9f0f56137
Jing <yanjing@yanjingdembp.home>
2020-03-01 16:49:48 -0500

    initial implementation

---
+++ src/nycproject/api.py
@@ -0,0 +1,9 @@
+from sodapy import Socrata
+
+def get_nycdata(app_key,page_size,num_pages):
+
+	client = Socrata("data.cityofnewyork.us",app_key)
+	results = []
+	for i in range(0,num_pages):
+		results.append(client.get('nc67-uf89',limit=page_size,offset=i*(page_size)))
+	return results
